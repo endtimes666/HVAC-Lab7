6664475992902761660ace9a060d6b98f5b01e04
eyefloater <gobifish@gmail.com>
2015-11-26 16:15:35 -0600

    .

---
+++ src/com/gaby/WaterHeater.java
@@ -0,0 +1,26 @@
+/*Fork and pull the HVAC service call program from
+HYPERLINK "https://github.com/minneapolis-edu/HVAC"https://github.com/minneapolis-edu/ 
+HYPERLINK "https://github.com/minneapolis-edu/HVAC" HVAC
+Review the Furnace class. There are a lot of static things in this class.
+Question: Identify all of the static things, and the reason why each is static.
+Problem 3:
+In your HVAC program, create a new class called WaterHeater. 
+This represents a service call for a water heater. 
+A water heater service call needs an address, date service requested, 
+description of the problem, and the age of the water heater. 
+Resolved service calls also need the resolved date, 
+description of the resolution, and the fee charged to the customer.
+The city requires that all service calls to water heaters 
+have a mandatory $20 extra charge added. As this applies to 
+all water heaters, add a static variable to your class to store this data.
+Add a toString method to WaterHeater which returns a string 
+containing all the static and instance variables for a WaterHeater. 
+You should break down the fee into the service charge plus the $20 mandatory city fee.
+Add code to HVAC.java to test your new class. 
+Make sure you can add service calls for water heaters to the list of todayServiceCalls.*/
+
+package com.gaby;
+
+public class WaterHeater {
+
+}
